0103d1c6ba33cf65ddf7674220da0e59d87e356c
JimmyC <jimcockerham@cox.net>
2025-01-18 07:39:57 -0700

    updated code, notes for team, JSON output, and CSVfiles
    Moved code to two Python files and executed API calls to create a JSON output. Executed script to create CSV files from data frames created using JSON output.

--- README.md
+++ README.md
@@ -32,19 +32,5 @@ To use the Recreation.gov API (RIDB API), you'll need an API key. This key authe
 
 The project's code is documented with comprehensive docstrings, making it easy to understand how each function works and can be used. You can access the docstrings in an interactive Python environment, or by using documentation generation tools such as sphinx.
 
-## Notes for commit Delete when refining readme.
-   - 1/17/2025 JC: removed print statement (No data found in API response) in the create_dataframe function to prevent unnecessary error messages in the console. 
 
-## Explanation for Data Analyst For Presentation
-"When we were designing how to store our data in the database, we had a few options on what data we would include in each table.
-We started by pulling all of the data from the facilities endpoint, and got a lot of fields in our result. We noticed that the API was also sending back three fields in the facilities response: ACTIVITY, CAMPSITE, and EVENT. However, these fields were empty lists, and it was confusing to include those in the data frame when we had separate data frames that contained the related data.
-When we first started, it was suggested that we remove these columns from the dataset. However, as we started using our data we realized that the original columns were helpful to have because we can use them as a high level indicator of whether or not a facility has related information without needing to join that table with another table. For instance, if you just wanted to check which facilities in Arizona had campsites you could quickly use df[df["CAMPSITES"].astype(bool)] and see what was available. So after some testing we decided it was useful to keep the original ACTIVITY, CAMPSITE, and EVENT columns in the facilities table even though they contained null values.
-For the campsites table, however, we initially started off by adding a column for PERMITTEDEQUIPMENT and ATTRIBUTES. These columns contained information that was organized as a list of dictionaries (for example: [{'EquipmentName': 'Tent', 'MaxLength': 40}, {'EquipmentName': 'RV', 'MaxLength': 35}]).
-Since we wanted to normalize our data, we decided to create new tables called PermittedEquipment and CampsiteAttributes that contained the information from these columns, so that they were no longer nested in the campsites table. Once we had done that, there was no longer a need to keep those nested values in our campsites table, and because of that, we dropped the PERMITTEDEQUIPMENT and ATTRIBUTES columns.
-By doing this we have normalized our data, reduced data redundancy, and created more efficient tables to store and manage data."
-Key Points to Emphasize for the Audience:
-•	Data Normalization: We were aiming to create a well-structured, efficient data model by separating related information into different tables.
-•	Redundancy: We avoided redundancy by not storing the same information in multiple tables.
-•	Use Cases: We made a decision on whether to keep a column or not based on what use cases we would have for them. If the column is useful and does not duplicate other information, it's a good idea to keep the column. If it is duplicated data, and does not provide an additional benefit, then it is best to remove it.
-•	Clarity: We wanted each table to contain a clear set of data and not be overly complex.
-•	Flexibility: By separating the information into different tables we now have more flexibility to extend the data model with new features in the future.
+
---
+++ create_dataframes.py
@@ -0,0 +1,169 @@
+import pandas as pd
+import json
+import os
+
+def create_dataframe(data, data_key="RECDATA"):
+    """Creates a pandas DataFrame from the extracted API data.
+
+    Args:
+        data: The list of dictionaries representing the data.
+        data_key: The key to use when accessing the data (default is 'RECDATA')
+
+    Returns:
+        A pandas DataFrame containing the API data or an empty DataFrame if there was an error.
+    """
+
+    if data:
+        return pd.DataFrame(data)
+    else:
+        return pd.DataFrame()
+    
+def process_facilities_data(facilities_data):
+    """Creates multiple DataFrames from the facilities data and related data.
+
+    Args:
+        facilities_data: The list of dictionaries representing the facilities from the API.
+
+    Returns:
+        A tuple containing the facilities_df, activities_df, campsites_df, and events_df.
+    """
+    if not facilities_data:
+        return pd.DataFrame(), pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
+    
+    facilities_df = create_dataframe(facilities_data)
+    
+    activities_data = []
+    campsites_data = []
+    events_data = []
+    
+    permitted_equipment_data = []  # Initialize permitted equipment data
+    campsite_attribute_db = []
+
+    for facility in facilities_data:
+        facility_id = facility.get('FacilityID')
+        if facility_id:
+            if 'ACTIVITY' in facility and facility["ACTIVITY"] : #Check if there is data
+                for act in facility["ACTIVITY"]:
+                    act['FacilityID'] = facility_id
+                    activities_data.append(act)
+
+            if 'CAMPSITE' in facility and facility["CAMPSITE"]: #Check if there is data
+              for camp in facility["CAMPSITE"]:
+                    camp_id = camp.get('CampsiteID')
+                    if camp_id:
+                        camp['FacilityID'] = facility_id #assign the facility id as a foreign key to be used in tables
+                        # Flatten Permitted Equipment data
+                        for equipment in camp.get('PERMITTEDEQUIPMENT', []):
+                             equipment['CampsiteID'] = camp_id
+                             permitted_equipment_data.append(equipment) # adds permitted equipment data into the list
+                        for attribute in camp.get('ATTRIBUTES', []):
+                            attribute['CampsiteID'] = camp_id
+                            campsite_attribute_db.append(attribute) #add the campsite attributes to the list
+                        campsites_data.append(camp)
+            if 'EVENT' in facility and facility["EVENT"]: #Check if there is data
+              for event in facility["EVENT"]:
+                event['FacilityID'] = facility_id
+                events_data.append(event)
+    
+    
+    facilities_df = facilities_df.drop(columns = ["ACTIVITY", "CAMPSITE", "EVENT"], errors = 'ignore') #Drop the columns that have blank data
+
+    activities_df = create_dataframe(activities_data)
+    campsites_df = create_dataframe(campsites_data)
+    events_df = create_dataframe(events_data)
+    permitted_equipment_df = create_dataframe(permitted_equipment_data) # Creates a DF for the permitted equipment
+    campsite_attributes_df = create_dataframe(campsite_attribute_db) # Creates a DF for the campsite attributes
+    
+
+    # Merge Permitted Equipment Back to the Campsite Table (using a left join)
+    #campsites_df = pd.merge(campsites_df, permitted_equipment_df, on="CampsiteID", how='left', suffixes=('', '_permit'))
+    #campsites_df = pd.merge(campsites_df, campsite_attributes_df, on="CampsiteID", how='left', suffixes = ('', '_attrib'))
+
+     # Drop nested columns from campsites table
+    campsites_df = campsites_df.drop(columns = ['ENTITYMEDIA' 'PERMITTEDEQUIPMENT', 'ATTRIBUTES','CreatedDate'], errors ='ignore')
+    facilities_df = facilities_df.drop(columns = ['FacilityAccessibilityText', 'Enabled', 'LINK', 'MEDIA', 'ORGANIZATION', 'PERMITENTRANCE', 'RECAREA', 'TOUR'], errors ='ignore')
+    activities_df = activities_df.drop(columns = ['FacilityActivityFeeDescription'], errors ='ignore')
+
+    return facilities_df, activities_df, campsites_df, events_df, permitted_equipment_df, campsite_attributes_df
+
+
+def output_to_csv(df, filename):
+    """Outputs a Pandas DataFrame to a CSV file.
+
+    Args:
+        df: The Pandas DataFrame to output.
+        filename: The name of the CSV file to create.
+    """
+    if not df.empty:
+      # Create the CSV Output Folder if it doesn't already exist
+      output_dir = 'csv_output'
+      if not os.path.exists(output_dir):
+          os.makedirs(output_dir) # Creates a new directory
+
+      output_file = os.path.join(output_dir, filename) # Create a path to output file
+      df.to_csv(output_file, index=False)
+      print(f"DataFrame successfully output to: {output_file}")
+    else:
+      print("Dataframe was empty and was not output to file.")
+
+
+if __name__ == "__main__":
+    # Load data from file
+    data_dir = 'json_output'
+    input_file = os.path.join(data_dir, "facilities_data.json")
+
+    try:
+      with open(input_file, 'r') as infile:
+        facilities_data = json.load(infile)
+    except FileNotFoundError:
+        print(f"Error: {input_file} not found. Please check that the script {input_file} was already run.")
+        exit()
+        
+
+    if facilities_data:
+        facilities_df, activities_df, campsites_df, events_df, permitted_equipment_df, campsite_attributes_df  = process_facilities_data(facilities_data)
+
+        if not facilities_df.empty:
+            print("Facilities DataFrame:")
+            facilities_df.info()
+            print(facilities_df.head())
+            output_to_csv(facilities_df, "facilities.csv")
+        else:
+            print("Failed to create the facilities Dataframe")
+        if not activities_df.empty:
+            print("\nActivities DataFrame:")
+            activities_df.info()
+            print(activities_df.head())
+            output_to_csv(activities_df, "activities.csv")
+        else:
+           print("\nFailed to create the activities Dataframe")
+        if not campsites_df.empty:
+            print("\nCampsites DataFrame:")
+            campsites_df.info()
+            print(campsites_df.head())
+            output_to_csv(campsites_df, "campsites.csv")
+        else:
+           print("\nFailed to create the campsites Dataframe")
+        if not events_df.empty:
+           print("\nEvents DataFrame:")
+           events_df.info()
+           print(events_df.head())
+           output_to_csv(events_df, "events.csv")
+        else:
+           print("\nEvents Dataframe empty, CSV not created")
+        if not permitted_equipment_df.empty:
+            print("\nPermitted Equipment DataFrame:")
+            permitted_equipment_df.info()
+            print(permitted_equipment_df.head())
+            output_to_csv(permitted_equipment_df, "permitted_equipment.csv")
+        else:
+           print("\nFailed to create the permitted equipment Dataframe")
+        if not campsite_attributes_df.empty:
+            print("\nCampsite Attributes DataFrame:")
+            campsite_attributes_df.info()
+            print(campsite_attributes_df.head())
+            output_to_csv(campsite_attributes_df, "campsite_attributes.csv")
+        else:
+           print("\nFailed to create the campsite attributes Dataframe")
+
+
